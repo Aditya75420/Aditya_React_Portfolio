baa7e4739ee25e4fac1005810f3c7efde8f8dacf
Aditya75420 <167458578+Aditya75420@users.noreply.github.com>
2025-08-15 11:01:24 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BsLinkedin } from 'react-icons/bs';
 import { FaGithub } from 'react-icons/fa';
-import { FaAngellist } from 'react-icons/fa'
+import { FaInstagram } from "react-icons/fa";
+
 import './footer.css';
 
 const Footer = () => {
@@ -13,19 +14,19 @@ const Footer = () => {
   
   return (
     <footer>
-      <a href="#home" className="footer__logo">Meri-MG</a>
+      <a href="#home" className="footer__logo">Aditya Kumar</a>
       <ul className="permalinks">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Skills</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
+{/*         <li><a href="#testimonials">Testimonials</a></li> */}
         <li><a href="#contact">Contact</a></li>
       </ul>
       <div className="footer__socials">
-      <a href="https://www.linkedin.com/in/meri-gogichashvili/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
-      <a href="https://github.com/Meri-MG" target="_blank" rel="noreferrer" ><FaGithub /></a>
-      <a href="https://angel.co/u/meri-gogichashvili" target="_blank" rel="noreferrer" ><FaAngellist /></a>
+      <a href="https://www.linkedin.com/in/aditya9576/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
+      <a href="https://github.com/Aditya75420" target="_blank" rel="noreferrer" ><FaGithub /></a>
+      <a href="https://www.instagram.com/itz_adittya11/?hl=en" target="_blank" rel="noreferrer" ><FaInstagram /></a>
       </div>
       <div className="footer__copyright">
         <small>&copy; ET {getYear()}. All rights reserved.</small>
