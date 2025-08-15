0a154320ddf1ed7ed3c43aedb9897c31598d57de
Aditya75420 <167458578+Aditya75420@users.noreply.github.com>
2025-08-15 11:48:45 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import CV from '../../assets/aditya_cv.pdf';
+// import CV from '../../assets/aditya_cv.pdf';
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
+      <a href={"/aditya_cv.pdf"} download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
